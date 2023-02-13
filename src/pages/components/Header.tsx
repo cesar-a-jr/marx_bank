332752c0fbc7f24636c96acede691a8a26fefda3
cesar-a-jr <cesaraugustojunior1234@gmail.com>
2023-02-13 20:07:59 -0300

    MarxBank

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,41 +1,59 @@
 import styles from "@/styles/Header.module.css";
 import Image from "next/image";
 
-import * as HoverCard from '@radix-ui/react-hover-card';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 import logo from "../../../public/favicon.ico";
+import { User } from "phosphor-react";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <div>
+        <div className={styles.aling}>
           <Image src={logo} alt="" />
-          <HoverCard.Root>
-            <HoverCard.Trigger asChild>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
               <button className={styles.Button} aria-label="Customise options">
                 MarxBank
               </button>
-            </HoverCard.Trigger>
+            </DropdownMenu.Trigger>
 
-            <HoverCard.Portal>
-              <HoverCard.Content
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
                 className={styles.DropdownMenuContent}
-                sideOffset={5}
               >
-                <div style={{ display: 'flex', flexDirection: 'column', gap: 7 }}>
-                <HoverCard.Content className={styles.DropdownMenuItem}>
-                  <button>Newsletter</button> 
-                </HoverCard.Content>
-                <HoverCard.Content className={styles.DropdownMenuItem}>
-                  <button>Blog4</button> 
-                </HoverCard.Content>
-                </div>
-              </HoverCard.Content>
-            </HoverCard.Portal>
-          </HoverCard.Root>
+                <DropdownMenu.Item  className={styles.DropdownMenuItem}>
+                 Newsletter
+                </DropdownMenu.Item >
+                <DropdownMenu.Item  className={styles.DropdownMenuItem}>
+                  Blog
+                </DropdownMenu.Item >
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+              <button className={styles.Button} aria-label="Customise options">
+                Conta Digital
+              </button>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
+                className={styles.DropdownMenuContent}
+              >
+                <DropdownMenu.Item  className={styles.DropdownMenuItem}>
+                 Criar sua Conta
+                </DropdownMenu.Item >
+                <DropdownMenu.Item  className={styles.DropdownMenuItem}>
+                  Taxas
+                </DropdownMenu.Item >
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
         </div>
-        <button></button>
+        <button className={styles.login}>Login <User size={24} /></button>
       </nav>
     </header>
   );
